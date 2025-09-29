7e4cc04695f99d8126b4f0d417b9f2385932b85d
Chaima Bouchareb <chaimabouchareb17@gmail.com>
2025-09-29 15:30:06 +0000

    ok

--- tests/test1.test.ts
+++ tests/test1.test.ts
@@ -1,1 +1,12 @@
-import {test} from '@playwright/test'
+import { test, expect } from '@playwright/test'
+test('login', async ({ page }) => {
+    await page.goto('https://playwright.dev/')
+    await expect(page).toHaveTitle(/Playwright/)
+});
+
+test('start', async({ page })=> {
+    await page.goto('https://playwright.dev/')
+    await page.getByRole('link', {name: 'Get started'}).click();
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+});
