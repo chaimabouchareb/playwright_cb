29492635f48f5a37877d8f2b8dafaaaf625dbad4
Chaima Bouchareb <chaimabouchareb17@gmail.com>
2025-10-30 16:55:57 +0100

    Delete tests/test2.spec.ts

--- tests/test2.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('basic test', async ({ page }) => {
-  await page.goto('https://chaimabouchareb.github.io/Testing-Pages/login_page.html');
-  await expect(page).toHaveTitle('Login');
-});
